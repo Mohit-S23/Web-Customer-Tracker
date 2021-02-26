da01f2f28d3d35fe3565d888c461438ae49d482f
Mohit-S23 <sharma.mohit0323@gmail.com>
2021-02-26 11:47:55 +0530

    Adding CustomerService layer

---
+++ Web-Customer-Tracker/src/com/mohit/spring/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.mohit.spring.service;
+
+import java.util.List;
+
+import com.mohit.spring.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+}
