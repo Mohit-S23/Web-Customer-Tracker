7ef6b47f834e2f2d385aba5a55db811ce2f21974
Mohit-S23 <sharma.mohit0323@gmail.com>
2021-02-26 15:54:37 +0530

    Search

--- Web-Customer-Tracker/src/com/mohit/spring/service/CustomerServiceImpl.java
+++ Web-Customer-Tracker/src/com/mohit/spring/service/CustomerServiceImpl.java
@@ -41,4 +41,11 @@ public class CustomerServiceImpl implements CustomerService {
 		customerDAO.deleteCustomer(theId);
 	}
 
+	@Override
+	@Transactional
+	public List<Customer> searchCustomers(String theSearchName) {
+		
+		return customerDAO.searchCustomers(theSearchName);
+	}
+
 }
